29c824c3523a0ae715a892a2bc1fc9f8bd93fe37
Gabriel Giordano <git@gabrielgiordano.com>
2019-04-04 11:51:19 -0300

    Add fields to Ecto Users and Posts tables

--- priv/repo/migrations/20190404144111_create_users_table.exs
+++ priv/repo/migrations/20190404144111_create_users_table.exs
@@ -2,6 +2,12 @@ defmodule BlogApi.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :subscription, :string
+      
+      timestamps()
+    end
   end
 end
--- priv/repo/migrations/20190404144118_create_posts_table.exs
+++ priv/repo/migrations/20190404144118_create_posts_table.exs
@@ -2,6 +2,15 @@ defmodule BlogApi.Repo.Migrations.CreatePostsTable do
   use Ecto.Migration
 
   def change do
-
+    create table(:posts) do
+      add :title, :string
+      add :body, :text
+      add :details, :map
+      add :user_id, references(:users, on_delete: :nothing)
+      
+      timestamps()
+    end
   end
+  
+  create index(:posts, [:user_id])
 end

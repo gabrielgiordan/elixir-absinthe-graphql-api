1fd8b6502372ba2d2e65cb23e248bea0e20b0f63
Gabriel Giordano <git@gabrielgiordano.com>
2019-04-05 14:31:57 -0300

    Add post_details to Blog types

--- lib/blog_api_web/types/blog_types.ex
+++ lib/blog_api_web/types/blog_types.ex
@@ -7,5 +7,12 @@ defmodule BlogApiWeb.Types.BlogTypes do
     field :title, :string
     field :body, :string
     field :user, :user, resolve: assoc(:user)
+    field :details, :post_detail
+  end
+
+  object :post_detail do
+    field :site, :string
+    field :section, :string
+    field :topic, :string
   end
 end

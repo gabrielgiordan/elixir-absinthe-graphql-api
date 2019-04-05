fbfe8d55b7b74f783459210336a3707d49c02221
Gabriel Giordano <git@gabrielgiordano.com>
2019-04-05 14:26:17 -0300

    Add GraphQL and API to the router

--- lib/blog_api_web/router.ex
+++ lib/blog_api_web/router.ex
@@ -1,11 +1,12 @@
 defmodule BlogApiWeb.Router do
   use BlogApiWeb, :router
+  alias BlogApiWeb.Schemas
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  forward "/api", Absinthe.Plug,
+    schema: Schemas.ApiSchema
 
-  scope "/api", BlogApiWeb do
-    pipe_through :api
-  end
+  forward "/graphql", Absinthe.Plug.GraphiQL,
+    schema: Schemas.ApiSchema,
+    interface: :playground
+
 end

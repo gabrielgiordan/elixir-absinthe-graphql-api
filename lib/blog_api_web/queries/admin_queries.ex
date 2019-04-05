14503382f4d6bdaf4a596b1a36d285f6262feab8
Gabriel Giordano <git@gabrielgiordano.com>
2019-04-05 13:40:01 -0300

    Add Admin and Blog queries

---
+++ lib/blog_api_web/queries/admin_queries.ex
@@ -0,0 +1,15 @@
+defmodule BlogApiWeb.Schemas.Queries.AdminQueries do
+  use Absinthe.Schema.Notation
+  alias BlogApiWeb.Resolvers.UserResolver
+
+  object :user_queries do
+    field :user, :user do
+      arg :id, non_null(:id)
+      resolve &UserResolver.find_user/3
+    end
+
+    field :users, list_of(:user) do
+      resolve &UserResolver.list_users/2
+    end
+  end
+end

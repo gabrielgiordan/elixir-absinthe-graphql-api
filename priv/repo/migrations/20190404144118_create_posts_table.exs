81b8ecc2a06e7248ab6523e1efaefe8354c62f40
Gabriel Giordano <git@gabrielgiordano.com>
2019-04-04 22:57:24 -0300

    Fix Post migration

--- priv/repo/migrations/20190404144118_create_posts_table.exs
+++ priv/repo/migrations/20190404144118_create_posts_table.exs
@@ -7,10 +7,10 @@ defmodule BlogApi.Repo.Migrations.CreatePostsTable do
       add :body, :text
       add :details, :map
       add :user_id, references(:users, on_delete: :nothing)
-      
+
       timestamps()
     end
+
+    create index(:posts, [:user_id])
   end
-  
-  create index(:posts, [:user_id])
 end
